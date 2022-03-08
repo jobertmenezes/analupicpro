4642717a887a80fd8d2d5a308b542dcc95a7b186
Jóbert Menezes <100235889+jobertmenezes@users.noreply.github.com>
2022-03-08 20:17:52 -0300

    dsds

---
+++ js/script.js
@@ -0,0 +1,45 @@
+//ADICIONAR IDS NOS BOTÕES
+//1...4
+//2 SELECIONAR OS BOTÕES
+//3 INTERCEPTAR O EVENTO DE CLIQUE
+//4 RECUPERAR O ID ADICIONADO
+
+//SELECIONANDO UL(LISTA)
+const listaOpcoes = document.querySelector(".secaoDetalhesConta__opcoesTransacoes")
+
+//ADICIONANDO ESCUTADOR DE EVENTOS
+listaOpcoes.addEventListener("click", indentificarOpcoes)
+
+//EXECUTANDO AÇÃO DEPOIS DO CLIQUE 
+function indentificarOpcoes(event){
+    
+    //IDENTIFICANDO ONDE OCORREU O CLIQUE
+    const elemento  = event.target
+    
+    //VERIFICANDO SE É UM ID
+    if(elemento.tagName == "LI") {
+
+        //RECUPERANDO O ID
+        const id = elemento.id
+        
+        //SELECIONANDO UMA SEÇÃO UTILIZANDO O ID(ESCOLHA DO USUARIO)
+        const secaoEscolha = document.querySelector(`div[data-id="${id}"]`)
+
+        //REMOCENDO CLASSE MOSTRAR
+        removeClasseMostrar()
+
+        //ADICIONAR UMA CLASSE
+        secaoEscolha.classList.add("mostrar")
+    }
+}
+
+//FUNÇÃO PARA REMOVER AS CLASSES DAS DIVS  
+function removeClasseMostrar(){
+    const divs = document.querySelectorAll(".secaoTransacao .container div")
+    
+    for(let i = 0; i < divs.length; i++){
+
+        divs[i].classList.remove("mostrar")
+
+    }
+}
